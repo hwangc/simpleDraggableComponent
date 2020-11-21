5121ec11c28155abb3af7b3b3c7aba2c95459051
HwangC <hwangc@hwangc.com>
2020-11-22 01:11:38 +0900

    Simple draggable component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
+function TopContainer(props) {
+  const dest = "top"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container top" onDrop={(evt) => props.drop(evt, dest)} onDragOver={(evt) => props.dragOver(evt)} >
+      <div className="header">Top container</div>
+      <div className="main" >
+        { props.box && <div id="top" draggable className="box" onDragStart={(evt) => props.dragStart(evt)}>Box</div> }
+      </div>
     </div>
-  );
+  )
+}
+
+function BottomContainer(props) {
+  const dest = "bottom"
+  return (
+    <div className="container bottom" onDrop={(evt) => props.drop(evt, dest)} onDragOver={(evt) => props.dragOver(evt)} >
+      <span className="header">Bottom container</span>
+      <div className="main">
+        { props.box && <div id="bottom" draggable className="box" onDragStart={(evt) => props.dragStart(evt)}>Box</div> }
+      </div>
+    </div>
+  )
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       isBoxOnTop: true 
+    }
+    this.onDrop = this.onDrop.bind(this)
+  }
+
+  onDrop(evt, dest) {
+    const startContainer = evt.dataTransfer.getData("text");
+    const dropContainer = dest
+    if (startContainer === dropContainer)
+      return
+    this.setState((state) => ({
+      isBoxOnTop: (startContainer === 'top') ? false: true
+    }))
+  }
+
+  onDragStart(evt) {
+    evt.dataTransfer.setData("text", evt.target.id);
+  }
+
+  onDragOver(evt) {
+    evt.preventDefault()
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <TopContainer box={this.state.isBoxOnTop} dragOver={this.onDragOver} dragStart={this.onDragStart} drop={this.onDrop}/>
+        <div className="divider"></div>
+        <BottomContainer box={!this.state.isBoxOnTop} dragOver={this.onDragOver} dragStart={this.onDragStart} drop={this.onDrop}/>
+      </div>
+    );
+  }
 }
 
 export default App;
